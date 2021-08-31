2160bda49839d77cb4a4b585e5ab742834b5622b
Palvin Wang <wangpalvin@gmail.com>
2021-08-30 19:26:53 -0500

    Your comment

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item">
+        <strong>Djackets
+          </strong>
+          </router-link>
+          <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+      </div>
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu}">
+        <div class="navbar-end">
+          <router-link to="/summer" class="navbar-item">Summer</router-link>
+          <router-link to="/winter" class="navbar-item">Winter</router-link>
+
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link to="/log-in" class="button is-right">Log in</router-link>
+
+              <router-link to="/cart" class="button is-success">
+                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                <span>Cart ({{ cartTotalLength }})</span>
+              </router-link>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+
+  <section class="section">
+      <router-view/>
+  </section>
+
+  <footer class="footer">
+    <p class="has-text-centered">Copyright (c) 2021</p>
+  </footer>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        showMobilMenu: false,
+        cart: {
+          items: []
+        }
+        }
+    },
+    beforeCreate() {
+      this.$store.commit('initializeStore')
+    },
+    computed: {
+      cartTotalLength() {
+        let totalLength = 0
+
+        for (let i = 0; i < this.cart.items.length; i++) {
+          totalLength += this.cart.items[i].quantity
+        }
+
+        return totalLength
+      }
+    }
+  }
+</script>
+
+
+<style lang="scss">
+@import '../node_modules/bulma';
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="home">
+
+    <!-- <div class="hero-body has-text-centered">
+            <p class="title mb-6">
+                Welcome to Djacket
+            </p>
+            <p class="subtitle">
+                The best jacket store online
+            </p>
+        </div> -->
+    <div id="slider">
+      <Slider />
+    </div>
+
+
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">Latest products</h2>
+      </div>
+
+      <ProductBox
+        v-for="product in latestProducts"
+        v-bind:key="product.id"
+        v-bind:product="product"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+import ProductBox from "@/components/ProductBox";
+import Slider from "@/components/Slider.vue";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      latestProducts: [],
+    };
+  },
+  components: {
+    ProductBox,
+    Slider,
+  },
+  mounted() {
+    this.getLatestProducts();
+
+    document.title = "Home | Djackets";
+  },
+  methods: {
+    async getLatestProducts() {
+      this.$store.commit("setIsLoading", true);
+
+      await axios
+        .get("/api/v1/latest-products/")
+        .then((response) => {
+          this.latestProducts = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+      this.$store.commit("setIsLoading", false);
+    },
+  },
+};
+</script>
+
